5a7de5f9c88789174cbe1f453026edc75f811478
Rupesh Kumar Chaudhary <rupeshjaiswal999939@gmail.com>
2026-07-05 15:39:17 +0530

    While loop

---
+++ Eleven.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    // int i=1;
+    // while(i<=10){
+    //     cout << "hello world" << i << endl;
+    //     i+=1;
+    // }
+
+    // int i=10;
+    // while(i>=1){
+    //     cout << i << endl;
+    //     i--;
+    // }
+
+    // int i=1;
+    // while(i<=10){
+    //     if(i==5){
+    //         i+=1;
+    //         continue;
+    //     }
+    //     cout << i << endl;
+    //     i+=1;
+    // }
+
+    int i=1;
+    while(i<=10){
+        if(i==3){
+            break;
+        }
+        cout << i << endl;
+        i++;
+    }
+    return 0;
+}
