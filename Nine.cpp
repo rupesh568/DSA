bbb3daa25197732cb3c01ddb792f2184e90ceb29
Rupesh Kumar Chaudhary <rupeshjaiswal999939@gmail.com>
2026-07-05 15:22:59 +0530

    String

---
+++ Nine.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string str="Rupesh";
+    cout << str[0] << endl;
+    cout << "length:" << str.size() << endl;
+    int len=str.size();
+    cout << str[len-1];
+    return 0;
+}
