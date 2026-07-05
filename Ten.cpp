fe422bdb0d0ed4baf156516f19572614cbe6345a
Rupesh Kumar Chaudhary <rupeshjaiswal999939@gmail.com>
2026-07-05 15:30:07 +0530

    For loop

---
+++ Ten.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+//For Loop
+int main(){
+    // for(int i=1;i<=10;i++){
+    //     cout << "Rupesh Kumar Chaudhary" << " "<< i << endl;
+    // }
+
+    for(int i=10;i>=1;i--){
+        cout << i << endl;
+    }
+
+    int n=2;
+    for(int i=1;i<=10;i++){
+        cout << i*n << endl;
+    }
+
+    for(int i=1;i<=10;i++){
+        if(i==5){
+            break;
+        }
+        cout << i << endl;
+    }
+
+    for(int i=1;i<=10;i++){
+        if(i==5){
+            continue;
+        }
+        cout << i << endl;
+    }
+    return 0;
+}
