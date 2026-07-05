0c6b7c6fd446d2e075ca9e74f51a191cc9c7d911
Rupesh Kumar Chaudhary <rupeshjaiswal999939@gmail.com>
2026-07-05 09:57:20 +0530

    Data types

---
+++ Second.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    //datatypes;
+    int a;
+    cin >> a;
+    cout << a << endl;
+    long b;
+    cin >> b;
+    cout << b << endl;
+    float c;
+    cin >> c;
+    cout << c << endl;
+    double d;
+    cin >> d;
+    cout << d << endl;
+    string str;
+    cin >> str;
+    cout << str << endl;
+    string str1;
+    getline(cin,str1);
+    cout << str1 << endl;
+
+    return 0;
+}
