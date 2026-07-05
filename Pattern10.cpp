6007ca8af2d042865ef178f3a8f06aa299b24138
Rupesh Kumar Chaudhary <rupeshjaiswal999939@gmail.com>
2026-07-06 00:28:09 +0530

    pattern10 completed

---
+++ Pattern10.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+void pattern10(int n){
+    int num=1;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout << num << " ";
+            if(num==1){
+                num=0;
+            }else{
+                num=1;
+            }
+            
+        }
+        
+
+        cout << endl;
+    }
+}
+int main(){
+    cout << "enter the value of num:";
+    int n;
+    cin >> n;
+    pattern10(n);
+    return 0;
+}
