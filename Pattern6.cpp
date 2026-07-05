775edca29e24538ed625da1b3e25251ccf9b0ebf
Rupesh Kumar Chaudhary <rupeshjaiswal999939@gmail.com>
2026-07-05 22:19:41 +0530

    pattern6

---
+++ Pattern6.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+void pattern6(int n){
+    for(int i=0;i<n;i++){
+        for(int j=1;j<n-i;j++){
+            cout << " ";
+        }
+        for(int j=0;j<=2*i;j++){
+            cout << "*";
+        }
+        for(int j=1;j<n-i;j++){
+            cout << " ";
+        }
+        cout << endl;
+    }
+}
+int main(){
+    cout << "enter the value of num:";
+    int n;
+    cin >> n;
+    pattern6(n);
+    return 0;
+}
