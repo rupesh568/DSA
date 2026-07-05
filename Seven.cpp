a4f1ffb9a08a8d241dbf0ae59e8672bc42ffc108
Rupesh Kumar Chaudhary <rupeshjaiswal999939@gmail.com>
2026-07-05 15:06:23 +0530

    1D array

---
+++ Seven.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//1D Array
+int main(){
+    int arr[5];
+    cin >> arr[0] >> arr[1] >> arr[2] >> arr[3] >>arr[4];
+    cout << arr[1] << endl;
+    arr[1]+=3;
+    cout << arr[1] << endl;
+
+    return 0;
+}
