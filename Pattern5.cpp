e7d3b412d5b2fb47452355bbae36407512d21d87
Rupesh Kumar Chaudhary <rupeshjaiswal999939@gmail.com>
2026-07-05 22:02:52 +0530

    pattern5 completed

---
+++ Pattern5.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+void pattern5(int n){
+    for(int i=0;i<n;i++){
+        for(int j=1;j<=n-i;j++){
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+int main(){
+    cout << "enter the value of n:";
+    int n;
+    cin >> n;
+    pattern5(n);
+    return 0;
+}
