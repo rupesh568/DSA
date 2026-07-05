d2c9ed98857555c48c388c6015b8b4848d8feecf
Rupesh Kumar Chaudhary <rupeshjaiswal999939@gmail.com>
2026-07-05 21:54:36 +0530

    pattern4 completed

---
+++ Pattern4.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+void pattern4(int n){
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            cout << "*" << " ";
+        }
+        cout << endl;
+    }
+}
+int main(){
+    cout << "enter the number:";
+    int n;
+    cin >> n;
+    pattern4(n);
+    return 0;
+}
