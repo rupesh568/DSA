f598615e855662e5f6d981a8c74194891155e75c
Rupesh Kumar Chaudhary <rupeshjaiswal999939@gmail.com>
2026-07-05 15:14:33 +0530

    2D Array

---
+++ Eight.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//2D ARRAY;
+int main(){
+    int arr[2][3];
+    cin >> arr[0][0] >> arr[0][1] >> arr[0][2]>>arr[1][0] >> arr[1] [1] >> arr[1][2];
+    cout << arr[0][0] << endl;
+    cout << arr[0] [1] << endl;
+
+    return 0;
+}
