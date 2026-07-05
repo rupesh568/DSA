bb63a92db72273584416fb4c7bbb49e94a87a282
Rupesh Kumar Chaudhary <rupeshjaiswal999939@gmail.com>
2026-07-05 23:31:12 +0530

    pattern9 completed

---
+++ Pattern9.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void pattern9(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i;j++){
+             cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+    cout << "enter the value of number:";
+    int n;
+    cin >>n;
+    pattern9(n);
+    return 0;
+}
