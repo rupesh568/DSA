3150037db9b730cf5110bdcaf5d651bce78e08a0
Rupesh Kumar Chaudhary <rupeshjaiswal999939@gmail.com>
2026-07-05 15:52:07 +0530

    parameterised function

---
+++ Fourteen.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+void printName(string name){
+    cout << "Hey " << name;
+}
+int main(){
+    string name;
+    getline(cin,name);
+    printName(name);
+    return 0;
+}
