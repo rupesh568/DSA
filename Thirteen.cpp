6998147a6f811cf4ba10cfa18c4f1d773f7f2ec8
Rupesh Kumar Chaudhary <rupeshjaiswal999939@gmail.com>
2026-07-05 15:49:58 +0530

    Paratemerised function

---
+++ Thirteen.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+//parametrised function;
+void printName(){
+    cout << "Rupesh Kumar Chaudhary" << endl;
+}
+int main(){
+    printName();
+
+    return 0;
+}
+
