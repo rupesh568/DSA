f3131eb98e438de1f67ed4ce74844fffd252c153
Rupesh Kumar Chaudhary <rupeshjaiswal999939@gmail.com>
2026-07-06 14:36:47 +0530

    STL-PAIR

---
+++ Nineteen.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    pair<int,int>p={1,2};
+    cout << p.first << endl;
+    cout << p.second << endl;
+
+    pair<int,pair<int,pair<int,int>>>p2={1,{3,{4,5}}};
+    cout << p2.first << endl;
+    cout << p2.second.second.second << endl;
+    cout << p2.second.second.first << endl;
+
+    pair<int ,int> arr[]={{1,2},{3,4},{5,6}};
+    cout << arr[0].first << endl;
+}
