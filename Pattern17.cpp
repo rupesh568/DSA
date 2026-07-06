74096019315f78991c9583ceaa45ddb71148cf73
Rupesh Kumar Chaudhary <rupeshjaiswal999939@gmail.com>
2026-07-06 12:26:05 +0530

    pattern17 completed

---
+++ Pattern17.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void pattern17(int n){
+    char ch='E';
+    for(int i=0;i<n;i++){
+        for(char j=ch;j<=ch+i;j++){
+            cout << j << " ";
+        }
+        ch=ch-1;
+        cout << endl;
+    }
+}
+
+int main(){
+    cout << "enter the value of number:";
+    int n;
+    cin >> n;
+    pattern17(n);
+    return 0;
+}
