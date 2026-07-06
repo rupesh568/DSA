91037b5ee0fe485d7f5d57ee1c8a6d2130227e98
Rupesh Kumar Chaudhary <rupeshjaiswal999939@gmail.com>
2026-07-06 12:59:02 +0530

    pattern19 completed

---
+++ Pattern19.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+void pattern19(int n){
+    for(int i=0;i<n-3;i++){
+        for(int j=0;j<=i+1-2*i;j++){
+            cout << "*";
+        }
+        for(int j=0;j<2*i;j++){
+            cout << " ";
+        }
+        for(int j=0;j<=i+1-2*i;j++){
+            cout << "*";
+        }
+        cout << endl;
+
+    }
+    for(int i=0;i<n-3;i++){
+        for(int j=0;j<=i;j++){
+            cout << "*";
+        }
+        for(int j=0;j<2-2*i;j++){
+            cout << " ";
+        }
+        for(int j=0;j<=i;j++){
+            cout << "*";
+        }
+        cout << endl;
+
+    }
+}
+
+int main(){
+    cout << "enter the value of number:";
+    int n;
+    cin >>n;
+    pattern19(n);
+    return 0;
+}
