0f47aadead205f81f219cbae8e8078a0100641e3
Rupesh Kumar Chaudhary <rupeshjaiswal999939@gmail.com>
2026-07-06 12:40:03 +0530

    pattern18 completed

---
+++ Pattern18.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+void pattern18(int n){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout << "*";
+        }
+        for(int j=0;j<2*n-2*i-2;j++){
+            cout << " ";
+        }
+        for(int j=0;j<=i;j++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            cout << "*";
+        }
+        for(int j=0;j<n-3+2*i;j++){
+            cout << " ";
+
+        }
+        for(int j=0;j<n-i-1;j++){
+            cout << "*";
+        }
+        cout << endl;
+
+    }
+}
+
+int main(){
+    cout << "enter the value of number:";
+    int n;
+    cin >> n;
+    pattern18(n);
+    return 0;
+}
