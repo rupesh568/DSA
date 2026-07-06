1f1b7dac5a0fc17e41e06b44996caa8872d88a21
Rupesh Kumar Chaudhary <rupeshjaiswal999939@gmail.com>
2026-07-06 10:16:07 +0530

    pattern13 completed

---
+++ Pattern13.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+void pattern13(int n){
+    
+    for(int i=0;i<n;i++){
+        for(char j='A';j<='A'+i;j++){
+            cout << j << " ";
+            
+        }
+        
+        cout << endl;
+    }
+}
+int main(){
+    cout << "enter the value of number:";
+    int n;
+    cin >> n;
+    pattern13(n);
+    return 0;
+    
+}
