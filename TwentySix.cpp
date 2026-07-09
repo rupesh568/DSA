4ef344270f0ba5d263995285cc8a17892e7191a5
Rupesh Kumar Chaudhary <rupeshjaiswal999939@gmail.com>
2026-07-09 09:42:16 +0530

    Set

---
+++ TwentySix.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+//set is used to store values in sorted order and unique elements;
+int main(){
+    set<int>st;
+    st.insert(1);
+    st.insert(4);
+    st.insert(3);
+    st.insert(3);
+    st.insert(5);
+
+    auto it=st.find(1);
+
+    st.erase(3);
+
+    int cnt=st.count(5);
+    cout << cnt << endl;
+
+    
+    
+
+    
+}
