d1e367ffcdc998d16ddc063e848f69818192e0b9
Rupesh Kumar Chaudhary <rupeshjaiswal999939@gmail.com>
2026-07-11 17:22:37 +0530

    Parameterised Recursion

---
+++ Recursion/Recursion8.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+void Recursion7(int val,int i,int N){
+    int sum=val;
+    
+    if(i>N){
+        cout << sum << endl;
+        return;
+    }
+    
+    sum+=i;
+    Recursion7(sum,i+1,N);
+    
+    
+    
+}
+int main(){
+    
+    Recursion7(0,1,5);
+    return 0;
+}
