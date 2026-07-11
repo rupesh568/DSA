52bbae8a6f5fe2a305dc172fb4dffbe89bbe84ad
Rupesh Kumar Chaudhary <rupeshjaiswal999939@gmail.com>
2026-07-11 14:11:43 +0530

    Recursion1

--- Recursion/Recursion1.cpp
+++ Recursion/Recursion1.cpp
@@ -1,5 +1,5 @@
 //Recursion is when a function calls itself multiple times unless a given condition is true;
-
+//so this function will run till infinite times why because there is no stopping condiiton ,so to stop this function we must have to use base case that will be used for stopping it
 #include<bits/stdc++.h>
 using namespace std;
 void print1(){
