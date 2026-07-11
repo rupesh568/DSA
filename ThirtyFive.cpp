cf5cdbc578ac2d7f1179d515b7f8c8847f3300ef
Rupesh Kumar Chaudhary <rupeshjaiswal999939@gmail.com>
2026-07-11 12:29:50 +0530

    Divisors

---
+++ ThirtyFive.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+void GCD(int N1,int N2){
+
+}
+int main(){
+    cout << "enter the numbers:";
+    int N1,N2;
+    cin >> N1,N2;
+    return 0;
+}
---
+++ ThirtySix.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+void findDivisors(int N){
+    vector<int>v;
+    // int i=1;
+    // while(i<=N){
+    //     if(N%i==0){
+    //         cout << i << endl;
+    //     }
+    //     i++;
+    // }
+    //we can do it using for loop as well ;
+    for(int i=1;i<=sqrt(N);i++){
+        if(N%i==0){
+            v.push_back(i);
+            if((N/i)!=i){
+                v.push_back(N/i);
+            }
+        }
+        
+    }
+    sort(v.begin(),v.end());
+    for(auto it:v){
+        cout << it << endl;
+    }
+}
+int main(){
+    cout << "enter the number:";
+    int N;
+    cin >> N;
+    findDivisors(N);
+
+    return 0;
+}
