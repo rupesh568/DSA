ba94f7cd3f11244f8d88ff4e8b904f8a4f11e015
Rupesh Kumar Chaudhary <rupeshjaiswal999939@gmail.com>
2026-07-11 13:33:22 +0530

    Hcf or Gcd

--- ThirtyEight.cpp
+++ ThirtyEight.cpp
@@ -1,10 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
-void 
+//Find GCD or HCF;
+//Time complexity:o(min(N1,N2))
+void checkGCD(int N1,int N2){
+    int gcd=1;
+    
+    for(int i=1;i<=min(N1,N2);i++){
+        if(N1%i==0 && N2%i==0){
+            gcd=i;
+            
+        }
+    }
+    cout << "Gcd:" << gcd;
+
+}
 
 int main(){
-    cout << "enter the number:";
-    int N;
-    cin >> N;
+    cout << "enter the numbers:";
+    int N1,N2;
+    cin >> N1 >> N2;
+    checkGCD(N1,N2);
     return 0;
 }
