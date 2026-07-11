5e33736d07f357df755a62aa51f165f2838f9f53
Rupesh Kumar Chaudhary <rupeshjaiswal999939@gmail.com>
2026-07-11 12:46:10 +0530

    Prime Number or not

---
+++ ThirtySeven.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+//check either number is prime or not:if a number is prime it has only two divisor ,so we will check its divisors;
+void checkPrime(int N){
+    vector<int>v;
+    int count=0;
+    for(int i=1;i<=N;i++){
+        if(N%i==0){
+            v.push_back(i);
+            count++;
+
+        }
+    }
+    sort(v.begin(),v.end());
+    for(auto it:v){
+        cout << it << endl;
+    }
+    if(count==2){
+        cout << "prime number" << endl;
+    }else{
+        cout << "not prime number" << endl;
+    }
+    
+}
+
+int main(){
+    cout << "enter the number:";
+    int N;
+    cin >> N;
+    checkPrime(N);
+    return 0;
+}
