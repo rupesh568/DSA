e58bbec717831265da5be92de0554eaa34879424
Rupesh Kumar Chaudhary <rupeshjaiswal999939@gmail.com>
2026-07-11 15:42:57 +0530

    Recursion using backtracking

--- Recursion/Recursion6.cpp
+++ Recursion/Recursion6.cpp
@@ -1,6 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
+//print  1 to N using backtracking ;That means we cannot use ++ method;
+void Recursion6(int start,int N){
+    if(start<1){
+        return;
+    }
+    
+    Recursion6(start-1,N);
+    cout << start << endl;
 
+}
+    
 int main(){
+    cout << "enter the numbers:";
+    int start,N;
+    cin >> start >> N;
+    Recursion6(start,N);
     return 0;
 }
