be73236f744db6f285525083b999775f50663d22
Rupesh Kumar Chaudhary <rupeshjaiswal999939@gmail.com>
2026-07-19 13:17:44 +0530

    Selection Sort

--- Hashing/MapString.cpp
+++ Hashing/MapString.cpp
@@ -5,8 +5,11 @@ int main(){
     int N;
     cin >> N;
     char arr[N];
-    map<char,int>mpp;
+    unordered_map<char,int>mpp; //always use unordered_map untill it work and use map only when it fails becuase in unordered map best case and average case happens which is O(1); which happens due to internal collision
+    //and another thing in unordered map the key must be a single data structure like int ,char,double but in map the key should be any datatypes as well as any datastructure
     //inserting values;
+
+    //Time complexity of unordered map:O(1) for best and average case but for worst case it is O(N) and time complexity of map is O(logN);
     for(int i=0;i<N;i++){
         cin >> arr[i];
         mpp[arr[i]]++;
---
+++ SortingAlgorithms/selectionsort.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+void selectionSort(int arr[],int N){
+    int i=0;
+    for(int i=0;i<N-1;i++){
+        int min_val=arr[i];
+        int k=i;
+        for(int j=i;j<N;j++){
+            if(arr[j]<min_val){
+                min_val=arr[j];
+                k=j;
+                
+                
+            }
+            
+        }
+        swap(arr[i],arr[k]);
+        
+        
+        
+    }
+    
+    for(int i=0;i<N;i++){
+        cout << arr[i];
+    }
+    
+}
+int main(){
+    int N;
+    cin >> N;
+    int arr[N];
+    for(int i=0;i<N;i++){
+        cin >> arr[i];
+    }
+    selectionSort(arr,N);
+    return 0;
+}
