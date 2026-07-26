c210f6a06b1cb74945aa696d1c63c3b11ba3fe3d
Rupesh Kumar Chaudhary <rupeshjaiswal999939@gmail.com>
2026-07-26 18:21:11 +0530

    merger sort completed

--- SortingAlgorithms/mergeSort.cpp
+++ SortingAlgorithms/mergeSort.cpp
@@ -1,13 +1,43 @@
 #include<bits/stdc++.h>
 using namespace std;
-void mergeSort(int arr[],int low,int high){
+
+void ms(int arr[],int low,int mid,int high){
     vector<int>v;
+    int left=low;
+    int right=mid+1;
+
+    while(left<=mid && right<=high){
+        if(arr[left]<=arr[right]){
+            v.push_back(arr[left]);
+            left++;
+        }
+        else{
+            v.push_back(arr[right]);
+            right++;
+        }
+
+    }
+    while(left<=mid){
+        v.push_back(arr[left]);
+        left++;
+    }
+    while(right<=high){
+        v.push_back(arr[right]);
+        right++;
+    }
+    for(int i=low;i<=high;i++){
+        arr[i]=v[i-low];
+    }
+}
+void mergeSort(int arr[],int low,int high){
+    
     if(low==high){
         return;
     }
     int mid=(low+high)/2;
     mergeSort(arr,low,mid);
     mergeSort(arr,mid+1,high);
+    ms(arr,low,mid,high);
     
 
 
@@ -35,5 +65,8 @@ int main(){
         cin >> arr[i];
     }
     mergeSort(arr,low,high);
+    for(int i=0;i<N;i++){
+        cout << arr[i];
+    }
     return 0;
 }
