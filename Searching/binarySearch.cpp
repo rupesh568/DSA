5b34856cd3d4ce41db13b568b46c5ae9b8417455
Rupesh Kumar Chaudhary <rupeshjaiswal999939@gmail.com>
2026-07-28 21:55:59 +0530

    Binary Search

---
+++ Searching/binarySearch.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+void binarySearch(int arr[],int K,int low,int high){
+    int mid=(low+high)/2;
+    if(arr[mid]==K){
+        cout << "Element is Found";
+    }else if(arr[mid]<K){
+        binarySearch(arr,K,mid+1,high);
+    }else if(arr[mid]>K){
+        binarySearch(arr,K,low,mid-1);
+    }
+}
+int main(){
+    int K;
+    cin >> K;
+    int N;
+    cin >> N;
+    int low=0;
+    int high=N-1;
+    int arr[N];
+    for(int i=0;i<N;i++){
+        cin >> arr[i];
+    }
+    binarySearch(arr,K,low,high);
+    return 0;
+}
