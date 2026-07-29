dfd6dd31e5eccced67cfb3ee3fb5efad7ec9e001
Rupesh Kumar Chaudhary <rupeshjaiswal999939@gmail.com>
2026-07-29 21:15:53 +0530

    Quick sort completed

--- SortingAlgorithms/quickSort.cpp
+++ SortingAlgorithms/quickSort.cpp
@@ -1,30 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
-void quickSort(int arr[],int low,int high){
-    int N=arr.size();
-    if(low<=high){
-        return;
-    }
-    int i=0;
-    int temp=arr[i];
-    int pivotElemet=arr[i];
-    for(int i=0;i<N;i++){
-        for(int j=high;j>0;j--){
-            if(i>j){
-                return;
-            }
-            if(arr[i]>arr[0] && arr[j]<arr[0]){
+int partition(int arr[],int low,int high){
+    int i=low;
+    int j=high;
+    while(i<j){
+        while(arr[i]<=arr[low] && i<=high-1){
+            i++;
+        }
+        while(arr[j]>=arr[low] && j>=low+1){
+            j--;
+        }
+        if(i<j){
             swap(arr[i],arr[j]);
         }
-        }
-        
-        
-        
     }
-    
-    quickSort(arr,low,j-1);
-    quickSort(arr,j+1,high);
+    swap(arr[j],arr[low]);
+    return j;
 }
+void quickSort(int arr[],int low,int high){
+    if(low<high){
+        int partitionIndex=partition(arr,low,high);
+        quickSort(arr,low,partitionIndex-1);
+        quickSort(arr,partitionIndex+1,high);
+    }
+
+    
+    
+}
+
 int main(){
     int N;
     cin >> N;
@@ -34,5 +37,8 @@ int main(){
          cin >> arr[i];
     }
     quickSort(arr,0,high);
+    for(int i=0;i<N;i++){
+        cout << arr[i] << " ";
+    }
     return 0;
 }
