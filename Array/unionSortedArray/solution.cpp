24051f03843dbf38f9049c1d1aea5a75918098c1
Rupesh Kumar Chaudhary <rupeshjaiswal999939@gmail.com>
2026-07-31 23:07:09 +0530

    union sort

--- Array/unionSortedArray/solution.cpp
+++ Array/unionSortedArray/solution.cpp
@@ -1,11 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<int> findUnion(vector<int> &a, vector<int> &b) {
+void findUnion(int a[],int b[],int N1,int N2) {
         vector<int>v;
         int i=0;
         int j=0;
-        while(i<a.size() && j<b.size()){
+        while(i<N1 && j<N2){
             
             if(a[i]<b[j]){
                 if(v.empty() || a[i]!=v[v.size()-1]){
@@ -35,7 +35,7 @@ vector<int> findUnion(vector<int> &a, vector<int> &b) {
                
             }
         }
-        while(i<a.size()){
+        while(i<N1){
             if(v.empty() || a[i]!=v[v.size()-1]){
                 v.push_back(a[i]);
                 i++;
@@ -44,7 +44,7 @@ vector<int> findUnion(vector<int> &a, vector<int> &b) {
             }
             
         }
-        while(j<b.size()){
+        while(j<N2){
             if(v.empty() || b[j]!=v[v.size()-1]){
                 v.push_back(b[j]);
                 j++;
@@ -53,17 +53,23 @@ vector<int> findUnion(vector<int> &a, vector<int> &b) {
             }
             
         }
-        return v;
+        for(int k=0;k<v.size();k++){
+            cout << v[k];
+        }
     }
 int main(){
-    int a;
-    cin >> a;
-    int N;
-    cin >> N;
-    int a[N];
-    for(int i=0;i<N;i++){
+    int N1;
+    cin >> N1;
+    int a[N1];
+    for(int i=0;i<N1;i++){
         cin >> a[i];
     }
-    int b;
+    int N2;
+    cin >> N2;
+    int b[N2];
+    for(int j=0;j<N2;j++){
+        cin >> b[j];
+    }
+    findUnion(a,b,N1,N2);
     
 }
