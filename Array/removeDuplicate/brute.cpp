47952e9fb8b3ffd1a9ef4ae5fa1d9ec8a6dae7db
Rupesh Kumar Chaudhary <rupeshjaiswal999939@gmail.com>
2026-08-01 15:18:14 +0530

    remove duplicate element-brute force

---
+++ Array/removeDuplicate/brute.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+void removeDuplicate(int arr[],int N){
+    int count=0;
+    set<int>st;
+    for(int i=0;i<N;i++){
+        st.insert(arr[i]);
+
+    }
+    int i=0;
+    
+    for(int it:st){
+        arr[i]=it;
+        i++;
+        count++;
+    }
+    for(int i=0;i<N;i++){
+        cout << arr[i];
+    }
+    cout << "No.of unique elements :" << count;
+}
+int main(){
+    int N;
+    cin >>N;
+    int arr[N];
+    for(int i=0;i<N;i++){
+        cin >> arr[i];
+    }
+    removeDuplicate(arr,N);
+}
