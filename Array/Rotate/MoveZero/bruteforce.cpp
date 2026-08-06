a14f88967a609b1d6166040f49973590e4ac06b0
Rupesh Kumar Chaudhary <rupeshjaiswal999939@gmail.com>
2026-08-06 23:54:08 +0530

    move zero to end by brute force

---
+++ Array/Rotate/MoveZero/bruteforce.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+void moveZero(int arr[],int N){
+    vector<int>v;
+    for(int i=0;i<N;i++){
+        if(arr[i]==0){
+            v.push_back(arr[i]);
+        }
+    }
+    int j=0;
+    for(int i=0;i<N;i++){
+        if(arr[i]!=0){
+            arr[j]=arr[i];
+            j++;
+        }
+    }
+    int l=1;
+    for(int i=0;i<v.size();i++){
+        arr[N-l]=v[i];
+        l++;
+    }
+    for(int i=0;i<N;i++){
+        cout << arr[i];
+    }
+}
+int main(){
+    int N;
+    cin >> N;
+    int arr[N];
+    for(int i=0;i<N;i++){
+        cin >> arr[i];
+    }
+    moveZero(arr,N);
+    return 0;
+}
