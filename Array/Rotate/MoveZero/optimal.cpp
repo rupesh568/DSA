c5ef1ebc69a3011fd755309d87223a2b54c9628d
Rupesh Kumar Chaudhary <rupeshjaiswal999939@gmail.com>
2026-08-07 23:01:02 +0530

    move zero

--- Array/Rotate/MoveZero/optimal.cpp
+++ Array/Rotate/MoveZero/optimal.cpp
@@ -1,7 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 void moveZero(int arr[],int N){
-    
+    for(int i=0;i<N;i++){
+        if(arr[i]==0){
+            int temp=arr[i];
+            while(i<N){
+                arr[i]=arr[i+1];
+                i++;
+            }
+            arr[N-1]=temp;
+            
+            
+        }
+    }
+    for(int i=0;i<N;i++){
+        cout << arr[i];
+    }
 }
 int main(){
     int N;
